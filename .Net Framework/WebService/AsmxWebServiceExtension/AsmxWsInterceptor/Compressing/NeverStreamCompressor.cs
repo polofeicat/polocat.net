9cc5a7d4dc7bed566c041c37315f6def9028cd00
polofeicat <raken3000@163.com>
2019-12-03 13:20:34 +0800

    cc1

--- .Net Framework/WebService/AsmxWebServiceExtension/AsmxWsInterceptor/Compressing/NeverStreamCompressor.cs
+++ .Net Framework/WebService/AsmxWebServiceExtension/AsmxWsInterceptor/Compressing/NeverStreamCompressor.cs
@@ -12,11 +12,26 @@ namespace AsmxWsInterceptor.Compressing
     /// </summary>
     internal class NeverStreamCompressor : StreamCompressor
     {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="outputStream"></param>
+        /// <param name="emptyBuffer"></param>
         protected override void CompressStream(Stream inputStream, Stream outputStream, byte[] emptyBuffer)
         {
             InternalNormalProcessStream(inputStream, outputStream, emptyBuffer);
         }
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="outputStream"></param>
+        /// <param name="emptyBuffer"></param>
         protected override void DecompressStream(Stream inputStream, Stream outputStream, byte[] emptyBuffer)
         {
             InternalNormalProcessStream(inputStream, outputStream, emptyBuffer);
